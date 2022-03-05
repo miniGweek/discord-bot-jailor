c55e318e3708e5015b92d4da321271443b2cbcb7
Rahul Sarkar <rsarkar@sxiq.com.au>
2022-03-06 07:43:32 +1100

    Add copy and deploy code to workflow

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This is used by github actions
+# do not use for deployment from local. Use deploy.ps1 for local deployment
+# scp main.py update_deployment.sh discord-bot-jailor.conf $1
+ 
+# ssh $1 'chmod +x /staging/update_deployment.sh && /staging/update_deployment.sh'
