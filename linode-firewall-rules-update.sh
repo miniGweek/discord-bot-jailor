c94813adb2c023da6dae8c9a509df9d18a00a776
Rahul Sarkar <rsarkar@sxiq.com.au>
2022-03-06 11:27:55 +1100

    updating shell to bash for github actions

--- linode-firewall-rules-update.sh
+++ linode-firewall-rules-update.sh
@@ -3,6 +3,13 @@
 echo "Updating linode firewall rules"
 
 # # firewall_rules_inbound='[{"action":"ACCEPT", "protocol": "TCP", "ports": "22", "label":"allow-SSH-from-github", "description":"Allow inbound ssh from github actions pipeline", "addresses": {"ipv4": ["192.0.2.1/32"]}},{"action":"ACCEPT", "protocol": "TCP", "ports": "22", "label":"allow-SSH-from-home", "description":"Allow inbound shh from home pc", "addresses": {"ipv4": ["8.39.18.208/32"]}}]'
+
+github_action_publicip=$(curl -l http://checkip.amazonaws.com/)
+
+echo $github_action_publicip
+publicip_json="\""$github_action_publicip"\""
+echo $publicip_json
+
  read -r -d '' firewall_rules_inbound << EOM
 [
     {
@@ -32,7 +39,6 @@ echo "Updating linode firewall rules"
 ]
 EOM
 
-echo "Going to update with the following json $firewall_rules_inbound"
 
 linode-cli firewalls rules-update 50400 \
 --inbound "$firewall_rules_inbound"
