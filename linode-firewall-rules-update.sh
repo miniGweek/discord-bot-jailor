2e2769724bad46bdc6935bc61a9b899a786cff00
Rahul Sarkar <rsarkar@sxiq.com.au>
2022-03-06 11:25:22 +1100

    updating shell to bash for github actions

--- linode-firewall-rules-update.sh
+++ linode-firewall-rules-update.sh
@@ -2,9 +2,35 @@
 
 echo "Updating linode firewall rules"
 
-# read -r -d '' firewall_rules_inbound << EOM
-firewall_rules_inbound='[{"action":"ACCEPT", "protocol": "TCP", "ports": "22", "label":"allow-SSH-from-github", "description":"Allow inbound ssh from github actions pipeline", "addresses": {"ipv4": ["192.0.2.1/32"]}},{"action":"ACCEPT", "protocol": "TCP", "ports": "22", "label":"allow-SSH-from-home", "description":"Allow inbound shh from home pc", "addresses": {"ipv4": ["8.39.18.208/32"]}}]'
-# EOM
+# # firewall_rules_inbound='[{"action":"ACCEPT", "protocol": "TCP", "ports": "22", "label":"allow-SSH-from-github", "description":"Allow inbound ssh from github actions pipeline", "addresses": {"ipv4": ["192.0.2.1/32"]}},{"action":"ACCEPT", "protocol": "TCP", "ports": "22", "label":"allow-SSH-from-home", "description":"Allow inbound shh from home pc", "addresses": {"ipv4": ["8.39.18.208/32"]}}]'
+ read -r -d '' firewall_rules_inbound << EOM
+[
+    {
+        "action": "ACCEPT",
+        "protocol": "TCP",
+        "ports": "22",
+        "label": "allow-SSH-from-github",
+        "description": "Allow inbound ssh from github actions pipeline",
+        "addresses": {
+            "ipv4": [
+                "192.0.2.1/32"
+            ]
+        }
+    },
+    {
+        "action": "ACCEPT",
+        "protocol": "TCP",
+        "ports": "22",
+        "label": "allow-SSH-from-home",
+        "description": "Allow inbound shh from home pc",
+        "addresses": {
+            "ipv4": [
+                "8.39.18.208/32"
+            ]
+        }
+    }
+]
+EOM
 
 echo "Going to update with the following json $firewall_rules_inbound"
 
