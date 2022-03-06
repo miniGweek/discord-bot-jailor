d6ffb1ff8c45e3cadb9e692b0589fd1bf3c5d0a9
Rahul Sarkar <rahul.git@outlook.com>
2022-03-06 12:31:33 +1100

    Add firewall rules to increase security (#1)
    * Testing linode cli auth and login
    
    * Test update linode-firewall rules
    
    * Test update linode-firewall rules
    
    * Test update linode-firewall rules
    
    * Test update linode-firewall rules
    
    * Test update linode-firewall rules
    
    * updating shell to bash for github actions
    
    * updating shell to bash for github actions
    
    * updating shell to bash for github actions
    
    * updating shell to bash for github actions
    
    * updating shell to bash for github actions
    
    * updating shell to bash for github actions
    
    * updating shell to bash for github actions
    
    * updating shell to bash for github actions
    
    * updating shell to bash for github actions
    
    * updating shell to bash for github actions
    
    * Adding github actions pipeline public ip to the firewall rules
    
    * refactoring firewall rules for reuse
    
    * refactoring firewall rules for reuse
    
    * refactoring firewall rules for reuse
    
    * refactoring firewall rules for reuse
    
    * refactoring firewall rules for reuse
    
    * refactoring firewall rules for reuse
    
    * removing echo of firewall rules to clean it up
    
    * removing echo of firewall rules to clean it up
    
    Co-authored-by: Rahul Sarkar <rsarkar@sxiq.com.au>

---
+++ linode-firewall-rules-update.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+echo "Updating linode firewall rules"
+
+# # firewall_rules_inbound='[{"action":"ACCEPT", "protocol": "TCP", "ports": "22", "label":"allow-SSH-from-github", "description":"Allow inbound ssh from github actions pipeline", "addresses": {"ipv4": ["192.0.2.1/32"]}},{"action":"ACCEPT", "protocol": "TCP", "ports": "22", "label":"allow-SSH-from-home", "description":"Allow inbound shh from home pc", "addresses": {"ipv4": ["8.39.18.208/32"]}}]'
+
+github_action_publicip=$(curl -l http://checkip.amazonaws.com/)
+
+echo "Github action pipeline runner public ip is : $github_action_publicip"
+
+publicip_in_json_acceptable_format="\""$github_action_publicip"/32\""
+
+echo "Github action pipeline runner public ip in /32 format is : $publicip_in_json_acceptable_format"
+
+read -r -d '' json_github_action_publicip_rule <<EOM
+{
+        "action": "ACCEPT",
+        "protocol": "TCP",
+        "ports": "22",
+        "label": "allow-SSH-from-github",
+        "description": "Allow inbound ssh from github actions pipeline",
+        "addresses": {
+            "ipv4": [
+                $publicip_in_json_acceptable_format
+            ]
+        }
+    }
+EOM
+
+read -r -d '' json_home_publicip_rule <<EOM
+{
+        "action": "ACCEPT",
+        "protocol": "TCP",
+        "ports": "22",
+        "label": "allow-SSH-from-home",
+        "description": "Allow inbound shh from home pc",
+        "addresses": {
+            "ipv4": [
+                "8.39.18.208/32"
+            ]
+        }
+    }
+EOM
+
+read -r -d '' common_deny_all_inbound_rule <<EOM
+{
+        "action": "DROP",
+        "protocol": "TCP",
+        "ports": "22",
+        "label": "deny-SSH-from-everywhere",
+        "description": "DROP/Deny inbound shh from everywhere else",
+        "addresses": {
+            "ipv4": [
+                "0.0.0.0/0"
+            ]
+        }
+    }
+EOM
+
+if [ "$1" == "addgithub-pip" ]; then
+    read -r -d '' firewall_rules_inbound <<EOM
+[
+    $json_github_action_publicip_rule, 
+    $json_home_publicip_rule,
+    $common_deny_all_inbound_rule
+]
+EOM
+
+elif [ "$1" == "removegithub-pip" ]; then
+    read -r -d '' firewall_rules_inbound <<EOM
+    [
+    $json_home_publicip_rule,
+    $common_deny_all_inbound_rule
+    ]
+EOM
+fi
+
+linode-cli firewalls rules-update 50400 \
+    --inbound "$firewall_rules_inbound" \
+    --json --pretty
+
+echo "Sleeping for 5s, allowing firewall rules to take effect"
+
+sleep 10
